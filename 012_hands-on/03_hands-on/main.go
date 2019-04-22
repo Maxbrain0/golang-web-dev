7bd23db85b6597376d68efcd758370336257ff5e
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-21 21:00:40 -0700

    Hands on 012-03

--- 012_hands-on/03_hands-on/main.go
+++ 012_hands-on/03_hands-on/main.go
@@ -1,6 +1,22 @@
 package main
 
-import "text/template"
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type hotel struct {
+	Name    string
+	Address string
+	City    string
+	Zip     int
+}
+
+type region struct {
+	Region string
+	Hotels []hotel
+}
 
 var tpl *template.Template
 
@@ -9,5 +25,61 @@ func init() {
 }
 
 func main() {
-
+	regions := []region{
+		region{
+			Region: "Southern",
+			Hotels: []hotel{
+				hotel{
+					Name:    "Cooltel",
+					Address: "12345 Coolton",
+					City:    "Apply Valley",
+					Zip:     12345,
+				},
+				hotel{
+					Name:    "Lametel",
+					Address: "54321 Lamelane",
+					City:    "San Diego",
+					Zip:     54321,
+				},
+			},
+		},
+		region{
+			Region: "Central",
+			Hotels: []hotel{
+				hotel{
+					Name:    "Fresno Inn",
+					Address: "1 Fresno Ave",
+					City:    "Fresno",
+					Zip:     91111,
+				},
+				hotel{
+					Name:    "Merced Suites",
+					Address: "111 Cow Lane",
+					City:    "Merced",
+					Zip:     91211,
+				},
+			},
+		},
+		region{
+			Region: "Northern",
+			Hotels: []hotel{
+				hotel{
+					Name:    "Seaside Dream",
+					Address: "1 Trinidad Coast Drive",
+					City:    "Trinidad",
+					Zip:     94768,
+				},
+				hotel{
+					Name:    "Y Not?",
+					Address: "512 Yreka Ct",
+					City:    "Yreka",
+					Zip:     95123,
+				},
+			},
+		},
+	}
+	err := tpl.Execute(os.Stdout, regions)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
